d28241b9125ffefa8ebe434924c46c44a640966d
2Heinz4Guenther6 <123951573+2Heinz4Guenther6@users.noreply.github.com>
2023-02-03 13:18:54 +0100

    Update Kartenstapel.java

--- Kartenstapel.java
+++ Kartenstapel.java
@@ -1,7 +1,25 @@
 
 public class Kartenstapel{
+    private List<Karte> karten;
+    private String typ;
+    private int spielerID;
     
-    public Kartenstapel(){
-        
+    public Kartenstapel(List<Karte> pKarten, String pTyp, int pID){
+        karten = pKarten;
+        typ = pTyp;
+        spielerID = pID;
+    }
+    
+    public void mischen(){
+        karten.toFirst();
+        int anzahl = 0;
+        while(!karten.isEmpty()){
+            karten.toFirst();
+        }
+    }
+    
+    public Karte getKarte(){
+        karten.toFirst();
+        return karten.getContent();
     }
 }

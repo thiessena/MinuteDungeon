3af079fa8ccd327cb9e7a600dc1bfd18913cc53e
Sadduzaer1 <124360051+Sadduzaer1@users.noreply.github.com>
2023-02-03 12:59:00 +0100

    Create Ressource.java

---
+++ Ressource.java
@@ -0,0 +1,15 @@
+public class Ressource
+{
+    public String Farbe;
+    public int Anzahl;
+    
+    public Ressource()
+    {
+        
+    }
+    
+    public int getAnzahl(int pAnzahl)
+    {
+        return pAnzahl;
+    }
+}

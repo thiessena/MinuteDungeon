609038b90bf6a88b3a5b3e44a2e6f7c888ac8caf
Leeee11 <123951115+Leeee11@users.noreply.github.com>
2023-02-03 12:57:22 +0100

    Create Spieler.java

---
+++ Spieler.java
@@ -0,0 +1,26 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Spieler.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Spieler
+{
+  
+    public Spieler()
+    {
+        
+    }
+
+   /**
+    *   public void KartenNachziehenNachziehstapel(){
+        Handkarten.add(Spieler.getNachziehstapel());
+        }
+    
+         public void KartenNachziehenAblagestapel(){
+        Handkarten.add(Spieler.getAblagestapel());
+        }
+    */
+  
+}

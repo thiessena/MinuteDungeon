c2a0621514cefa6a5d59691214d2925000a227cf
philippkuehl <123951187+PhilippKuehl@users.noreply.github.com>
2023-02-03 13:17:18 +0100

    Create DeckEreigniskarte.java

---
+++ DeckEreigniskarte.java
@@ -0,0 +1,35 @@
+public class DeckEreigniskarte extends Ereigniskarte
+{
+    private String Name;
+
+    public DeckEreigniskarte()
+    {
+
+    }
+
+    public void Chaos()
+    {
+    }
+    
+    public void einWehWeh()
+    {
+        
+    }
+    
+    public void plötzlichKrankheit()
+    {
+        
+    }
+    
+    public void hinterhalt()
+    {
+    
+    }
+    
+    public void falltür()
+    {
+        
+    
+    }
+}
+

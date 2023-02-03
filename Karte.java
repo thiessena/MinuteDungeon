c8d81d3187cb9fa516d706f4d1ccd46dd3761b55
2Heinz4Guenther6 <123951573+2Heinz4Guenther6@users.noreply.github.com>
2023-02-03 13:17:07 +0100

    Update Karte.java

--- Karte.java
+++ Karte.java
@@ -1,8 +1,20 @@
 public class Karte{
- 
-  public Karte(){
-    
-  }
-  
-  
+    private int x ;
+    private int y;
+    private boolean verdeckt;
+    private String Farbe;
+  public Karte(){
+  }
+  public int getX(){
+      return x;
+  }
+  public int getY(){
+      return y;
+  }
+  public void setX(int pX){
+    x = pX;
+  }
+  public void setY(int pY){
+      y = pY;
+  }
 }

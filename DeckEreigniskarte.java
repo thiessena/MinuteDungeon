b452b5029ea4257f8dc723548ef5acd4bfabcdd0
thiessena <a.thiessen@ahfs-detmold.de>
2023-02-07 22:35:35 +0100

    DeckEreigniskarte: aktion Spieler: + Handkarten

--- DeckEreigniskarte.java
+++ DeckEreigniskarte.java
@@ -4,10 +4,10 @@ public class DeckEreigniskarte extends Ereigniskarte
     //Hallo Philipp, die Karte Chaos ist eigentlich der Name. 
     //D.h. du kannst eine neue Karte Chaos erstellen: 
     public static final DeckEreigniskarte CHAOS = new DeckEreigniskarte("Chaos", "JEDER gibt seine Handkarten einem Mitspieler.");
-    public static final Deckereigniskarte EINWEHWEH = new DeckEreigniskarte("Ein Wehweh", "JEDER legt 1 Karte auf den eigenen Ablagestapel");
-    public static final Deckereigniskarte PLOETZLICHKRANKHEIT = new DeckEreigniskarte("Plötzlich Krankheit!", "JEDER legt alle Handkarten auf den eigenen Ablagestapel");
-    public static final Deckereigniskarte HNTERHALT = new DeckEreigniskarte("Hinterhalt!", "Deckt 2 Karten aus dem Dungeon auf. Ihr müsst beide besiegen, bevor es weitergeht");
-    public static final Deckereigniskarte FALLTUER = new DeckEreigniskarte("Falltür", "JEDER legt 3 Karten auf den eigenen Ablagestapel");
+    public static final DeckEreigniskarte EINWEHWEH = new DeckEreigniskarte("Ein Wehweh", "JEDER legt 1 Karte auf den eigenen Ablagestapel");
+    public static final DeckEreigniskarte PLOETZLICHKRANKHEIT = new DeckEreigniskarte("Plötzlich Krankheit!", "JEDER legt alle Handkarten auf den eigenen Ablagestapel");
+    public static final DeckEreigniskarte HNTERHALT = new DeckEreigniskarte("Hinterhalt!", "Deckt 2 Karten aus dem Dungeon auf. Ihr müsst beide besiegen, bevor es weitergeht");
+    public static final DeckEreigniskarte FALLTUER = new DeckEreigniskarte("Falltür", "JEDER legt 3 Karten auf den eigenen Ablagestapel");
 
 
     public DeckEreigniskarte(String pName, String pBeschreibung)
@@ -18,26 +18,28 @@ public class DeckEreigniskarte extends Ereigniskarte
 
     public void aktion(DeckEreigniskarte pDE){
         switch (pDE.name){
-            "Chaos": //toDo -> Veränderung des Spiels
+            case "Chaos": //toDo -> Veränderung des Spiels
                  
                 List<Spieler> sListe = Spiel.getInstance().getSpieler();
-                Handkarten temp = sListe.toFirst().getContent().getHandkarten();
+                sListe.toFirst();
+                Kartenstapel temp = sListe.getContent().getHandkarten();
                 sListe.toFirst();
                 Spieler bekommt = sListe.getContent();
                 sListe.next();
                 while(sListe.hasAccess()){
                     Spieler von = sListe.getContent();
-                    
+                    bekommt.setHandkarten(von.getHandkarten());
+                    bekommt = von;
+                    sListe.next();
                 }
+                bekommt.setHandkarten(temp);
             break;
-            "Ein Wehweh": 
+            case "Ein Wehweh":
+            //toDo: Hallo Luca, ich habe die obere Karte zu Ende gemacht. 
+            //      Meine Idee: Du kannst den Quelltext oben verstehen und dann 
+            //      auf die anderen DeckEreigniskarten anwenden. 
             break;
         }
-    
     }
-    
-   
-    
-    
 }
 

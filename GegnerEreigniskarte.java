2240069f2793db574fa0b4418e6543101d440b25
philippkuehl <123951187+PhilippKuehl@users.noreply.github.com>
2023-02-10 13:17:15 +0100

    Update GegnerEreigniskarte.java
    GegnerEreigniskarte überarbeitet

--- GegnerEreigniskarte.java
+++ GegnerEreigniskarte.java
@@ -1,17 +1,25 @@
 public class GegnerEreigniskarte extends Ereigniskarte
 {
     private String gegnertyp;
+    private Gegnerkarte gegnerkarte;
 
-    public GegnerEreigniskarte(String pName, String pBeschreiung){
-        super(pName, pBeschreiung);
+    public static final GegnerEreigniskarte Rücklings = new GegnerEreigniskarte ("Rücklings", "Besiege eine Person");
+    public static final GegnerEreigniskarte Feuerball = new GegnerEreigniskarte ("Feuerball", "Besiege 1 Monster");
+    public static final GegnerEreigniskarte Treffer = new GegnerEreigniskarte ("Treffer", "Besiege 1 Person");
+    public static final GegnerEreigniskarte Haudrauf = new GegnerEreigniskarte ("Haudrauf", "Besiege 1 Monster");
+    public static final GegnerEreigniskarte HeiligeHandgranate = new GegnerEreigniskarte ("Heilige Handgranate!", "Besiege eine beliebige Karte");
+
+    public GegnerEreigniskarte(String pName, String pBeschreibung)
+    {
+
     }
 
-    public void beseitigen(Gegnerkarte gK){
-        if(gegnertyp.equals(gK.getTyp())){
+    public void beseitigen(Gegnerkarte gegnerkarte){
+        if(gegnertyp.equals(gegnerkarte.getTyp())){
             System.out.println("Gegner beseitigt!");
-        }
-        else{
-            System.out.println("Gegner kann mit der Ereigniskarte nicht beseitigt werden!");
+    }
+     else{
+          System.out.println("Gegner kann mit der Ereigniskarte nicht beseitigt werden!");
         }
     }
 }

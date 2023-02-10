3bf7ac6b7c3f1e3c9a0d699a1b21678718b3cb84
Andyaya <123951100+Andyaya@users.noreply.github.com>
2023-02-10 13:18:36 +0100

    Create Held.java

---
+++ Held.java
@@ -0,0 +1,25 @@
+public class Held
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private String heldenname
+    private String faehigkeitname;
+    
+
+    /**
+     * Konstruktor für Objekte der Klasse Held
+     */
+    public Held()
+    {
+        // Instanzvariable initialisieren
+        
+    }
+
+
+     
+    public void faehigkeit()
+    {
+        //Ehm also diese methode soll den Spieler weiterleiten, damit wir uns einfach über die
+        //haufen verschiedener Helden zugriff
+        
+    }
+}

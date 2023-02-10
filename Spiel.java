bd1b79fc22fabfd636aaabf2166ee8e86da1db2f
hammhamm1 <124357770+hammhamm1@users.noreply.github.com>
2023-02-10 13:15:53 +0100

    Update Spiel.java

--- Spiel.java
+++ Spiel.java
@@ -6,6 +6,9 @@
 public class Spiel {
     private static final Spiel instance = new Spiel();
     List<Spieler> spieler; 
+    Dungeon dungeon;
+    Countdown countdown;
+    
     /**
      * Hier fehlen noch all die Attribute, die für das Spiel benötigt werden.
      */
@@ -22,32 +25,33 @@ public class Spiel {
      * Zeigt einen Bildschirm zur Auswahl des Charakters. 
      */
     public void charakterAuswahl(){
-
+        spieler = new List<spieler>();
     }
 
+    public void schwierigkeitAuswahl(){
+    
+        dungeon = new dungeon(1,1);// Level, Schwierigkeitsgrad
+    }
+    
     /**
      * Initialisiert das Spiel mit dem ersten Level. 
      * //toDo: Später weitere Level ergänzen
      */
     public void init(){
         //toDo: Alle Ereignisse für den Start des Spiels
+        //Für jeden Spieler
+        //  Nachziehstapel mischen
+        
+        //Timer starten
     }
-
-    /**
-     * Öffnet die nächste Gegnerkarte und erlaubt das Legen von Karten
-     */
-    public void naechsteGegner(){
-
-    }
-
-    /**
-     * Startet den Boss-Fight. 
-     * toDo:
-     * - Musik ändern
-     * - Hintergrund ändern
-     */
-    public void boss(){
-
+    
+    public void ablauf(){
+        while(!dungeon.getGewonnen() && countdown.ende()){
+            //dungeon anzeigen
+            //countdown anzeigen
+            //spieler anzeigen
+        }
+        
     }
 
     /**
@@ -61,5 +65,11 @@ public class Spiel {
 
     public List<Spieler> getSpieler(){
         return spieler;
+    }
+    public Dungeon getDungeon(){
+        return dungeon;
+    }
+    public Countdown getCountdown(){
+        return countdown;
     }
 }

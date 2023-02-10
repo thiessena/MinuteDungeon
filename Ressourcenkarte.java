11ca614db2cfdbe97c06c013ba6841d7d1d6366c
Sadduzaer1 <124360051+Sadduzaer1@users.noreply.github.com>
2023-02-10 13:41:44 +0100

    Update Ressourcenkarte.java

--- Ressourcenkarte.java
+++ Ressourcenkarte.java
@@ -1,25 +1,28 @@
 public class Ressourcenkarte extends Karte
 {
   private Ressource schild;
-  private int schwert;
-  private int sprung;
-  private int schriftrolle;
-  private int pfeil;
+  private Ressource schwert;
+  private Ressource sprung;
+  private Ressource schriftrolle;
+  private Ressource pfeil;
   
   public Ressourcenkarte(int pSchild, int pSchwert, int pSprung, int pSchriftrolle, int pPfeil)
   {
     schild = new Ressource("gelb", pSchild);
-  
+    schwert = new Ressource("rot", pSchwert);
+    sprung = new Ressource("lila", pSprung);
+    schriftrolle = new Ressource("blau", pSchriftrolle);
+    pfeil = new Ressource("grün", pPfeil);
   }
   
   public int[] getArray()
   {
     int[]ressource = new int[5];
     ressource[0] = schild.getAnzahl();
-    ressource[1] = pSchwert;
-    ressource[2] = pSprung;
-    ressource[3] = pSchriftrolle;
-    ressource[4] = pPfeil; 
+    ressource[1] = schwert.getAnzahl();
+    ressource[2] = sprung.getAnzahl();
+    ressource[3] = schriftrolle.getAnzahl();
+    ressource[4] = pfeil.getAnzahl(); 
     return ressource;
   }
 }

37b9c600e4a182883874e0a1bb0c1a2d44e9e08b
StinckyMe <123951094+StinckyMe@users.noreply.github.com>
2023-02-10 12:45:09 +0100

    Update Gegnerkarte.java

--- Gegnerkarte.java
+++ Gegnerkarte.java
@@ -1,12 +1,23 @@
-public class Gegnerkarte {
-    
-    private String typ; 
-
-    public Gegnerkarte(String pTyp){
-        typ = pTyp;
+/**
+ * V1
+ */
+public class GegenerKarte
+{
+    private String gegnerName;
+    private Ressourcenkarte benötigteRessourcen;
+    private String gegenerTyp;
+    /**
+     * Konstruktor für Objekte der Klasse GegenerKarte
+     */
+    public GegenerKarte(String pName,Ressourcenkarte pR,String pTyp)
+    {
+        gegnerName = pName;
+        benötigteRessourcen = pR;
+        gegenerTyp = pTyp;
     }
-
-    public String getTyp(){
-        return typ;
+    public void kampf(){
+        //irgentwie die Ressourcenkarte lesen und ein Array erstellen 
+        //irgentwie Karten von Spieler entfangen und die in das Array eintragen 
+        //dann prüfen ob alles da ist 
     }
 }

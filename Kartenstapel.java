73c68c8cc17fb25e02c0e4d88f65c96e57c0b6dd
2Heinz4Guenther6 <123951573+2Heinz4Guenther6@users.noreply.github.com>
2023-02-10 13:15:29 +0100

    Update Kartenstapel.java

--- Kartenstapel.java
+++ Kartenstapel.java
@@ -2,30 +2,32 @@ public class Kartenstapel{
     private List<Karte> karten;
     private String typ;
     private int spielerID;
+    private int anzahl;
     
     public Kartenstapel(List<Karte> pKarten, String pTyp, int pID){
         karten = pKarten;
         typ = pTyp;
         spielerID = pID;
+        zaehlen();
     }
     
     public void mischen(){
         int p = 0;
         while(p < 1000){
             karten.toFirst();
-            Karte temp = karten.getContent()
-            int random = (int)(Math.random()*(karten.size() + 1))
+            Karte temp = karten.getContent();
+            int random = (int)(Math.random()*(anzahl + 1));
             for(int i = 0; i < random; i++){
                 karten.next();
             }
-            insert(temp);
+            karten.insert(temp);
             p++;
         }
     }
     
     /**
-     * Gibt die oberste Karte des Kartenstapels zurück. 
-     * Falls es keine Karte im Stapel gibt, wird null zurückgegeben.
+     * Gibt die oberste Karte des Kartenstapels zurÃ¼ck. 
+     * Falls es keine Karte im Stapel gibt, wird null zurÃ¼ckgegeben.
      * @return
      */
     public Karte getObersteKarte(){
@@ -34,7 +36,7 @@ public class Kartenstapel{
     }
 
     /**
-     * Gibt eine Liste mit n Karten vom Stapel, falls der Karten soviele Karten enthält.
+     * Gibt eine Liste mit n Karten vom Stapel, falls der Karten soviele Karten enthÃ¤lt.
      * @param n
      * @return
      */
@@ -43,7 +45,7 @@ public class Kartenstapel{
         int p = 0;
         karten.toFirst();
         while(p < n){
-            Karte neu = karten.getObject();
+            Karte neu = karten.getContent();
             temp.append(neu);
             karten.next();
             p++;
@@ -68,5 +70,11 @@ public class Kartenstapel{
         karten.insert(pKarte);
     }
 
-
+    public void zaehlen(){
+        karten.toFirst();
+        while(karten.hasAccess()){
+            anzahl++;
+            karten.next();
+        }
+    }
 }

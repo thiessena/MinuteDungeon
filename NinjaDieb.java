573a25d26c40f80a262d8e433a0acf2fea16f050
Andyaya <123951100+Andyaya@users.noreply.github.com>
2023-02-17 13:00:41 +0100

    Create NinjaDieb.java

---
+++ NinjaDieb.java
@@ -0,0 +1,21 @@
+/**
+ * Beschreiben Sie hier die Klasse NinjaDieb.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class NinjaDieb
+{
+   
+    /**
+     * Konstruktor für Objekte der Klasse NinjaDieb
+     * Faehigkeit: 3x karten ablegenen und dafür besiege ein Hindernis
+     */
+    public NinjaDieb()
+    {
+        // Instanzvariable initialisieren
+      
+    }
+
+ 
+}

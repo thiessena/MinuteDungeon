f940adbc5fcd4477eed9fe09f5a18e1ea335279a
Andyaya <123951100+Andyaya@users.noreply.github.com>
2023-02-17 12:58:47 +0100

    Create PaladinWalkuere.java

---
+++ PaladinWalkuere.java
@@ -0,0 +1,20 @@
+/**
+ * Beschreiben Sie hier die Klasse PaladinWalkuere.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class PaladinWalkuere
+{
+ 
+    /**
+     * Konstruktor für Objekte der Klasse PaladinWalkuere
+     * Faehigkeiten: 3x Karten ablegen und dafür können alle anderenzwei karten ziehen
+     */
+    public PaladinWalkuere()
+    {
+        
+    }
+
+    
+}

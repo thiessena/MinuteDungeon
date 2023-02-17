53b6d3a4e27c17b8c62f000ff0e0e818b47e9b63
Andyaya <123951100+Andyaya@users.noreply.github.com>
2023-02-17 13:01:14 +0100

    Create ZauberinMagier.java

---
+++ ZauberinMagier.java
@@ -0,0 +1,21 @@
+/**
+ * Beschreiben Sie hier die Klasse ZauberinMagier.
+ * 
+ * 
+ */
+public class ZauberinMagier
+{
+   
+
+    /**
+     * Konstruktor für Objekte der Klasse ZauberinMagier
+     *  Faehigekeit: 3x Karten ablegen und überwinde ein Hindernis
+     */
+    
+    public ZauberinMagier()
+    {
+       
+    }
+
+    
+}

ca0c7c615ec5e5099e523d69ba20f5101562d4c1
StinckyMe <123951094+StinckyMe@users.noreply.github.com>
2023-02-17 13:00:54 +0100

    Update Dungeon.java

--- Dungeon.java
+++ Dungeon.java
@@ -16,11 +16,8 @@ public class Dungeon
         //prüfenObNochKartenDaSind wenn ja dann Karte von oben aus dem Gegenerkartestappel aufdecken 
     }
     
-    public void kämpfen(){
-        //Recurcen vom gegener zeigen und die die schon im Array sind 
-        //get Recurcen welche der Gegener braucht um besiet werden muss und array machen wo der Spieler seine recucen rein machen kann
-        //Nur Recurcen rein lassen die überein stimmen wenn alle da sind dann Karte weglegen
-    }
+   public void RessourcenKarteVergleichen(){
+        GegnerKarte.arrayGegner();
     
     public void kartenWeglegen(){
         //besiegte Karte wegglegen und die da zugehörigen Reurcen u. Array und dann prüfen ob noch karten da sind 

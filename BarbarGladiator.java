73b9b2b58b36fb96fe668326c6d663829f5a2b65
Andyaya <123951100+Andyaya@users.noreply.github.com>
2023-02-17 13:00:06 +0100

    Create BarbarGladiator.java

---
+++ BarbarGladiator.java
@@ -0,0 +1,20 @@
+/**
+ * Beschreiben Sie hier die Klasse BabarGladiator.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class BabarGladiator
+{
+
+    /**
+     * Konstruktor für Objekte der Klasse BabarGladiator
+     * Faehigkeit: 3x Karten aablegen und besiege eine Person
+     */
+    public BabarGladiator()
+    {
+        
+    }
+
+    
+}

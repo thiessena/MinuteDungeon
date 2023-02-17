bd53c3a1dba70eac0958b82ea45da08fc5bd9d97
Andyaya <123951100+Andyaya@users.noreply.github.com>
2023-02-17 12:59:37 +0100

    Create jaegerinWaldleufer.java

---
+++ jaegerinWaldleufer.java
@@ -0,0 +1,21 @@
+
+/**
+ * Beschreiben Sie hier die Klasse JaegerinWaldlaeufer.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class JaegerinWaldlaeufer
+{
+   
+    /**
+     * Konstruktor für Objekte der Klasse JaegerinWaldlaeufer
+     * 3x Karten ablegen und besiege eine person.
+     */
+    public JaegerinWaldlaeufer()
+    {
+
+    }
+
+    
+}

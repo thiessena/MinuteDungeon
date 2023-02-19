9c7262d178f65fb2ff7bf2650e8a5c1b271ac3d8
thiessena <a.thiessen@ahfs-detmold.de>
2023-02-19 19:30:59 +0100

    Start für ein Textspiel

---
+++ textgame/Textgame.java
@@ -0,0 +1,20 @@
+package textgame;
+import karten.*;
+import main.*;
+
+import java.util.Scanner;
+
+public class Textgame {
+    Spiel spiel = Spiel.getInstance();
+    Scanner eingabe; 
+
+    public Textgame(){
+
+    }
+
+
+    public static void main(String[] args){
+        Textgame t = new Textgame(); 
+        
+    }
+}

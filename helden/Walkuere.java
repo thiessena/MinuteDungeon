54a388610d3cbef42f57b2ab9f2132095c3e07b2
Andyaya <123951100+Andyaya@users.noreply.github.com>
2023-03-03 12:30:43 +0100

    Create Walkuere.java

---
+++ helden/Walkuere.java
@@ -0,0 +1,24 @@
+package helden;
+/**
+ * Beschreiben Sie hier die Klasse Walkuere.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Walkuere
+{
+ 
+    /**
+     * Konstruktor fuer Objekte der Klasse Walkuere
+     * Faehigkeiten: 3x Karten ablegen und dafuer koennen alle anderenzwei karten ziehen
+     */
+    public Walkuere()
+    {
+        /**
+         * [3x public void karteRauslegen(Karte pKarte)] + 2x public void karteHinzufuegen
+         */
+        
+    }
+
+    
+}

cc780bfa20e02af1e7da34fb909aecd03215f856
Andyaya <123951100+Andyaya@users.noreply.github.com>
2023-03-03 12:28:41 +0100

    Create Ninja.java

---
+++ helden/Ninja.java
@@ -0,0 +1,24 @@
+package helden;
+/**
+ * Beschreiben Sie hier die Klasse Ninja.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Ninja
+{
+   
+    /**
+     * Konstruktor fÃ¼r Objekte der Klasse Ninja
+     * Faehigkeit: 3x karten ablegenen und dafÃ¼r besiege ein Hindernis
+     */
+    public Ninja()
+    {
+        /**
+         * 3x public void karteHinzufuegen + set  gegnerTyp hindernis = 0;
+         */
+      
+    }
+
+ 
+}

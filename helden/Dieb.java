a0e38952e5f5ae42db0eb1fc62eaa46eb51fc345
Andyaya <123951100+Andyaya@users.noreply.github.com>
2023-03-03 12:27:56 +0100

    Update and rename NinjaDieb.java to Dieb.java

--- helden/Dieb.java
+++ helden/Dieb.java
@@ -1,18 +1,18 @@
 package helden;
 /**
- * Beschreiben Sie hier die Klasse NinjaDieb.
+ * Beschreiben Sie hier die Klasse Dieb.
  * 
  * @author (Ihr Name) 
  * @version (eine Versionsnummer oder ein Datum)
  */
-public class NinjaDieb
+public class Dieb
 {
    
     /**
-     * Konstruktor fÃ¼r Objekte der Klasse NinjaDieb
+     * Konstruktor fÃ¼r Objekte der Klasse Dieb
      * Faehigkeit: 3x karten ablegenen und dafÃ¼r besiege ein Hindernis
      */
-    public NinjaDieb()
+    public Dieb()
     {
         /**
          * 3x public void karteHinzufuegen + set  gegnerTyp hindernis = 0;

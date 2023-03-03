ad74caba9fa1621ed1437c28db0c1c96f6b54778
Wancy-Neiss <123951109+Wancy-Neiss@users.noreply.github.com>
2023-03-03 12:40:41 +0100

    Update NetTrennzeichen.java

--- netzwerk/NetTrennzeichen.java
+++ netzwerk/NetTrennzeichen.java
@@ -12,4 +12,5 @@ public class NetTrennzeichen {
     public static final String KARTEN_TRENNZEICHEN = "%";
     public static final String SPIEL_TRENNZEICHEN = "#";
     public static final String SPIELER_TRENNZEICHEN = "/";
+    public static final String SPIELER_TRENNZEICHEN = "§";
 }

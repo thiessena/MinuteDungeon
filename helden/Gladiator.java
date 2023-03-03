56117cc0e537492269548842bd7084e0e70b3d5f
Andyaya <123951100+Andyaya@users.noreply.github.com>
2023-03-03 12:25:46 +0100

    Update and rename BarbarGladiator.java to Gladiator.java

--- helden/Gladiator.java
+++ helden/Gladiator.java
@@ -1,18 +1,18 @@
 package helden;
 /**
- * Beschreiben Sie hier die Klasse BabarGladiator.
+ * Beschreiben Sie hier die Klasse Gladiator.
  * 
  * @author (Ihr Name) 
  * @version (eine Versionsnummer oder ein Datum)
  */
-public class BabarGladiator
+public class Gladiator
 {
 
     /**
-     * Konstruktor fÃ¼r Objekte der Klasse BabarGladiator
+     * Konstruktor fÃ¼r Objekte der Klasse Gladiator
      * Faehigkeit: 3x Karten ablegen und besiege eine Person
      */
-    public BabarGladiator()
+    public Gladiator()
     {
         /**
          * [3x public void karteRauslegen(Karte pKarte)] + set  gegnerTyp, person = 0;

f06dfdeea3f09835d643a02e88fa7334be2dd5fa
thiessena <a.thiessen@ahfs-detmold.de>
2023-03-06 13:21:46 +0100

    Kommentar hinzugefuegt

--- helden/Held.java
+++ helden/Held.java
@@ -1,55 +1,50 @@
 package helden;
+
 import main.Spieler;
 import netzwerk.NetObject;
 
-public class Held implements NetObject
-{
+public class Held implements NetObject {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
     protected String heldenname;
     protected String faehigkeitname;
     protected Spieler spieler;
-    
 
-    public Held(){
-        
+    public Held() {
+
     }
 
     /**
      * Konstruktor für Objekte der Klasse Held
      */
-    public Held(String pHeldenname)
-    {
+    public Held(String pHeldenname) {
         heldenname = pHeldenname;
-        
+
     }
 
+    /**
+     * Wird vom konkreten Helden uberschrieben.
+     */
+    public void nutzefaehigkeit() {
 
-     
-    public void nutzefaehigkeit(){
-        
     }
 
-    public void setSpieler(Spieler pSpieler){
+    public void setSpieler(Spieler pSpieler) {
         spieler = pSpieler;
     }
-
 
     public String getHeldenname() {
         return heldenname;
     }
 
-    public String toString(){
-        return heldenname; 
+    public String toString() {
+        return heldenname;
     }
-
 
     @Override
     public void fromNetString(String pNetString) {
         // TODO Auto-generated method stub
-        
+
     }
-
-
 
     @Override
     public String toNetString() {

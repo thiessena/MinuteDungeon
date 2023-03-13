75e063ed8f8d852463d7c234bbb0ec79424ccc56
Andyaya <123951100+Andyaya@users.noreply.github.com>
2023-03-13 12:49:13 +0100

    Create waldlaeufer.java

---
+++ helden/waldlaeufer.java
@@ -0,0 +1,24 @@
+package helden;
+
+/**
+ * Beschreiben Sie hier die Klasse JaegerinWaldlaeufer.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Waldlaeufer
+{
+   
+    /**
+     * Konstruktor fÃ¼r Objekte der Klasse JaegerinWaldlaeufer
+     * 3x Karten ablegen und besiege eine person.
+     */
+    public waldlaeufer()
+    {
+        /**
+         * [3x public void karteRauslegen(Karte pKarte)] + set  gegnerTyp, person = 0;
+         */
+    }
+
+    
+}

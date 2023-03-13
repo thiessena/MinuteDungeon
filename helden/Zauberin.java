5ee5fb0f85afe68410bfcadb8e892d4557136f3f
Andyaya <123951100+Andyaya@users.noreply.github.com>
2023-03-13 13:17:56 +0100

    hab ein Konzept zur faehigkeit mehtode

--- helden/Zauberin.java
+++ helden/Zauberin.java
@@ -1,24 +1,29 @@
-package helden;
-/**
- * Beschreiben Sie hier die Klasse ZauberinMagier.
- * 
- * 
- */
 public class Zauberin
 {
    
 
     /**
-     * Konstruktor fÃ¼r Objekte der Klasse ZauberinMagier
-     *  Faehigekeit: 3x Karten ablegen und Ã¼berwinde ein Hindernis
+     * Konstruktor fÃƒÂ¼r Objekte der Klasse ZauberinMagier
+     *  Faehigekeit: 3x Karten ablegen und ÃƒÂ¼berwinde ein Hindernis
      */
     
     public Zauberin()
     {
        /**
         * 3x public void karteHinzufuegen + set  gegnerTyp hindernis = 0;
+        * 
+        * Konzept
+        * 
+         * getSliste.getGegenerkarte.getTyp
+         * if(getTyp == "Hindernis" ){
+         *      setRescourcen == 0;
+            }
+         * else
+         *  system.out.println ("Kannst deine Fähigkeit nicht bei diesem Typ anwenden")
+         *
         */
     }
 
     
 }
+
